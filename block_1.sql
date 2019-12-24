5c1d31502623de94b5a3f23cb790cc843cb62abd
konst187 <konst-den20@mail.ru>
2019-12-24 16:22:50 +0300

    fixed primary key for films

--- block_1.sql
+++ block_1.sql
@@ -18,7 +18,11 @@ CREATE TABLE `Actors` (
   `firstname` varchar(255) NOT NULL,
   `surname` varchar(255) DEFAULT NULL,
   `date` date NOT NULL
-)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `Actors`
+--
 
 INSERT INTO `Actors` (`act_id`, `firstname`, `surname`, `date`) VALUES
 (1, 'Ivan', 'Dulin', '1976-11-13'),
@@ -26,129 +30,235 @@ INSERT INTO `Actors` (`act_id`, `firstname`, `surname`, `date`) VALUES
 (3, 'Чувак', 'Чувачело', '1990-11-04'),
 (4, 'Брюс', 'Вылез', '1965-09-03');
 
-ALTER TABLE `Actors` 
-  ADD PRIMARY KEY (`act_id`); -- Уникальный ID актера
+--
+-- Индексы сохранённых таблиц
+--
 
-ALTER TABLE `Actors` 
+--
+-- Индексы таблицы `Actors`
+--
+ALTER TABLE `Actors`
+  ADD PRIMARY KEY (`act_id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `Actors`
+--
+ALTER TABLE `Actors`
   MODIFY `act_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
--- Таблица кинотеатров
+--
+-- Структура таблицы `C_role`
+--
+
+CREATE TABLE `C_role` (
+  `role_id` int(11) NOT NULL,
+  `name` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `C_role`
+--
+
+INSERT INTO `C_role` (`role_id`, `name`) VALUES
+(1, 'Актер'),
+(2, 'Режиссер');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `C_role`
+--
+ALTER TABLE `C_role`
+  ADD PRIMARY KEY (`role_id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `C_role`
+--
+ALTER TABLE `C_role`
+  MODIFY `role_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+--
+-- Структура таблицы `Cinema`
+--
+
 CREATE TABLE `Cinema` (
   `name` varchar(11) NOT NULL,
   `address` varchar(11) NOT NULL
-)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `Cinema`
+--
 
 INSERT INTO `Cinema` (`name`, `address`) VALUES
 ('Восход', 'Восточная'),
 ('Заря', 'Северная'),
 ('Звезда', 'Южная');
 
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `Cinema`
+--
 ALTER TABLE `Cinema`
-  ADD PRIMARY KEY (`name`); -- Уникальное имя кинотеатра
+  ADD PRIMARY KEY (`name`);
 COMMIT;
 
--- Таблица возможных ролей 
-CREATE TABLE `C_role` (
-  `role_id` int(11) NOT NULL,
-  `name` varchar(25) NOT NULL
-)
-
-INSERT INTO `C_role` (`role_id`, `name`) VALUES
-(1, 'Актер'),
-(2, 'Режиссер');
-
-ALTER TABLE `C_role`
-  ADD PRIMARY KEY (`role_id`); -- Уникальный ID роли
-
-ALTER TABLE `C_role`
-  MODIFY `role_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-COMMIT;
-
--- Таблица фильмов
 CREATE TABLE `Film` (
+  `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `genre` varchar(255) NOT NULL,
   `length` time(6) NOT NULL,
   `country` varchar(255) NOT NULL,
   `age` int(255) NOT NULL,
   `company` varchar(255) NOT NULL
-)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `Film` (`name`, `genre`, `length`, `country`, `age`, `company`) VALUES
-('Avengers', 'Fantasy', '02:00:00.000000', 'USA', 16, 'WeedCorp'),
-('Snatch', 'Comedy', '02:02:00.000000', 'USA', 18, 'CocaineSmashers'),
-('TAXI', 'Comedy', '02:03:00.000000', 'France', 14, 'WeedCorp'),
-('Terminator', 'Action', '01:54:00.000000', 'USA', 16, 'DolbitDigital');
+--
+-- Дамп данных таблицы `Film`
+--
 
-ALTER TABLE `Film` -- Уникальное название фильма
-  ADD PRIMARY KEY (`name`);
+INSERT INTO `Film` (`id`, `name`, `genre`, `length`, `country`, `age`, `company`) VALUES
+(1, 'Terminator', 'Action', '02:00:00.000000', 'USA', 16, 'USAMARINCORP'),
+(2, 'Snatch', 'Comedy', '01:54:00.000000', 'USA', 18, 'WeedCorp'),
+(3, 'Taxi', 'Comedy', '02:22:00.000000', 'France', 12, 'FrancePD');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `Film`
+--
+ALTER TABLE `Film`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `Film`
+--
+ALTER TABLE `Film`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
--- Таблица расписания фильмов для кинотеатров
+--
+-- Структура таблицы `Film_actors`
+--
+
+CREATE TABLE `Film_actors` (
+  `Film` int(11) NOT NULL,
+  `Actor` int(255) NOT NULL,
+  `role` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `Film_actors`
+--
+
+INSERT INTO `Film_actors` (`Film`, `Actor`, `role`) VALUES
+(2, 1, 1),
+(3, 1, 1),
+(1, 4, 1),
+(1, 3, 2),
+(3, 2, 1),
+(3, 3, 1);
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `Film_actors`
+--
+ALTER TABLE `Film_actors`
+  ADD KEY `role` (`role`),
+  ADD KEY `Film` (`Film`),
+  ADD KEY `Actor` (`Actor`);
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `Film_actors`
+--
+ALTER TABLE `Film_actors`
+  ADD CONSTRAINT `Film_actors_ibfk_1` FOREIGN KEY (`role`) REFERENCES `C_role` (`role_id`),
+  ADD CONSTRAINT `Film_actors_ibfk_2` FOREIGN KEY (`Film`) REFERENCES `Film` (`id`),
+  ADD CONSTRAINT `Film_actors_ibfk_3` FOREIGN KEY (`Actor`) REFERENCES `Actors` (`act_id`);
+COMMIT;
+
+--
+-- Структура таблицы `schedule`
+--
+
 CREATE TABLE `schedule` (
   `id` int(11) NOT NULL,
   `hall` int(255) NOT NULL,
   `date` datetime(6) NOT NULL,
-  `film` varchar(255) NOT NULL,
-  `Zone` varchar(255) NOT NULL, -- Что-то типа такого?
+  `film_id` int(11) NOT NULL,
+  `Zone` varchar(255) NOT NULL,
   `price` int(255) NOT NULL,
   `cinema` varchar(255) NOT NULL
-)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `schedule` (`id`, `hall`, `date`, `film`, `Zone`, `price`, `cinema`) VALUES
-(1, 3, '2019-11-13 00:00:00.000000', 'Snatch', 'VIP', 200, 'Звезда'),
-(2, 2, '2019-11-28 00:00:00.000000', 'Terminator', 'VIP', 300, 'Восход'),
-(3, 6, '2019-11-30 00:00:00.000000', 'Terminator', 'REGULAR', 310, 'Заря'),
-(4, 1, '2019-11-27 00:00:00.000000', 'Avengers', 'VIP', 228, 'Восход'),
-(5, 4, '2019-12-02 00:00:00.000000', 'Terminator', 'REGULAR', 200, 'Звезда'),
-(6, 2, '2019-12-24 00:00:00.000000', 'Terminator', 'REGULAR', 300, 'Восход'),
-(7, 1, '2019-12-25 00:00:00.000000', 'Terminator', 'VIP', 310, 'Восход'),
-(8, 1, '2019-12-18 00:00:00.000000', 'Terminator', 'REGULAR', 228, 'Заря');
+--
+-- Дамп данных таблицы `schedule`
+--
 
+INSERT INTO `schedule` (`id`, `hall`, `date`, `film_id`, `Zone`, `price`, `cinema`) VALUES
+(1, 2, '2019-12-30 00:00:00.000000', 2, 'VIP', 310, 'Восход'),
+(2, 2, '2019-12-24 00:00:00.000000', 1, 'VIP', 200, 'Заря'),
+(3, 5, '2019-12-11 00:00:00.000000', 3, 'Regular', 228, 'Звезда'),
+(4, 1, '2020-01-01 00:00:00.000000', 2, 'VIP', 301, 'Восход');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `schedule`
+--
 ALTER TABLE `schedule`
-  ADD PRIMARY KEY (`id`), -- Уникальный id
-  ADD KEY `film` (`film`),  -- Внешний ключ для связки с таблицей фильмов
-  ADD KEY `Cinema` (`cinema`); -- Внешний ключ для связки с таблицей кинотеатров
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `film` (`film_id`),
+  ADD KEY `Cinema` (`cinema`);
 
-ALTER TABLE `schedule`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
 
+--
+-- AUTO_INCREMENT для таблицы `schedule`
+--
 ALTER TABLE `schedule`
-  ADD CONSTRAINT `schedule_ibfk_1` FOREIGN KEY (`film`) REFERENCES `Film` (`name`), -- Связка с таблицей фильмов
-  ADD CONSTRAINT `schedule_ibfk_2` FOREIGN KEY (`cinema`) REFERENCES `Cinema` (`name`); -- Связка с таблицей кинотеатров
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `schedule`
+--
+ALTER TABLE `schedule`
+  ADD CONSTRAINT `schedule_ibfk_1` FOREIGN KEY (`film_id`) REFERENCES `Film` (`id`),
+  ADD CONSTRAINT `schedule_ibfk_2` FOREIGN KEY (`cinema`) REFERENCES `Cinema` (`name`);
 COMMIT;
-
--- Создание дополнительной таблицы для реализации связи многие ко многим (Фильм - актер - роль)
-CREATE TABLE `Film_actors` (
-  `Film` varchar(255) NOT NULL,
-  `Actor` int(255) NOT NULL,
-  `role` int(255) NOT NULL
-)
-
-ALTER TABLE `Film_actors`
-  ADD KEY `role` (`role`), -- Внешний ключ для связки с таблицей ролей
-  ADD KEY `Film` (`Film`), -- Внешний ключ для связки с таблицей фильмов
-  ADD KEY `Actor` (`Actor`); -- Внешний ключ для связки с таблицей актеров
-
-ALTER TABLE `Film_actors`
-  ADD CONSTRAINT `Film_actors_ibfk_1` FOREIGN KEY (`role`) REFERENCES `C_role` (`role_id`), -- Связка с таблицей ролей
-  ADD CONSTRAINT `Film_actors_ibfk_2` FOREIGN KEY (`Film`) REFERENCES `Film` (`name`), -- Связка с таблицей фильмов 
-  ADD CONSTRAINT `Film_actors_ibfk_3` FOREIGN KEY (`Actor`) REFERENCES `Actors` (`act_id`); -- Связка с таблицей актеров
-COMMIT;
-
--- заполнение по внешним ключам таблицы:
-INSERT INTO `Film_actors` (`Film`, `Actor`, `role`) VALUES
-('Avengers', 1, 1),
-('Avengers', 2, 2),
-('Snatch', 2, 1),
-('TAXI', 4, 1),
-('Snatch', 3, 1),
-('Terminator', 3, 1),
-('Terminator', 2, 1);
-
-
-
-
-
-
--- block_2_3.sql
+++ block_2_3.sql
@@ -1,4 +1,5 @@
 -- 30.Выберите все кинотеатры, показывающие в текущем месяце фильм «Терминатор».
 --  SELECT schedule.cinema FROM `schedule` WHERE `schedule`.`film` = "Terminator" and `schedule`.`date` BETWEEN  '2019-11-1' AND '2019-11-31' 
 -- V.2.0
-SELECT DISTINCT schedule.cinema FROM `schedule` WHERE `schedule`.`film` = "Terminator" and MONTH(`schedule`.`date`) = MONTH(NOW()) AND YEAR(`schedule`.`date`) = YEAR(NOW())
+SELECT DISTINCT schedule.cinema FROM `schedule` WHERE `schedule`.`id` = "1" and MONTH(`schedule`.`date`) = MONTH(NOW()) AND YEAR(`schedule`.`date`) = YEAR(NOW());
+
