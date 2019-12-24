886fcf394bfad2587b1ff7b433809c986fcb528e
konst187 <konst-den20@mail.ru>
2019-12-24 11:32:46 +0300

    2.3 fixed

--- block_2_3.sql
+++ block_2_3.sql
@@ -1,2 +1,4 @@
 -- 30.Выберите все кинотеатры, показывающие в текущем месяце фильм «Терминатор».
- SELECT schedule.cinema FROM `schedule` WHERE `schedule`.`film` = "Terminator" and `schedule`.`date` BETWEEN  '2019-11-1' AND '2019-11-31'
+--  SELECT schedule.cinema FROM `schedule` WHERE `schedule`.`film` = "Terminator" and `schedule`.`date` BETWEEN  '2019-11-1' AND '2019-11-31' 
+-- V.2.0
+SELECT DISTINCT schedule.cinema FROM `schedule` WHERE `schedule`.`film` = "Terminator" and MONTH(`schedule`.`date`) = MONTH(NOW()) AND YEAR(`schedule`.`date`) = YEAR(NOW())
