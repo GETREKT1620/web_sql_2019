31cdda728bc52f55e213878b3d55e6e08ad7e533
GETREKT1620 <48867064+GETREKT1620@users.noreply.github.com>
2020-01-22 17:39:13 +0300

    Update block_2_2.sql

--- block_2_2.sql
+++ block_2_2.sql
@@ -1,2 +1,6 @@
 -- 29.Выберите для каждой кинокомпании (название) количество снятых ею фильмов.
 SELECT Film.company, COUNT(*) FROM `Film` WHERE Film.company IS NOT NULL GROUP BY Film.company
+
+-- or
+
+SELECT Film.company, SUM(CASE WHEN Film.company is NULL THEN 0 ELSE 1 END) as cout FROM `Film` GROUP BY Film.company
